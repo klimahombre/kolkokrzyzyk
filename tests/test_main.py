2f1cb57abca64fb15b5e540852987464299388ae
olax2669 <ola1236610@wp.pl>
2025-07-04 10:44:15 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,3 +17,23 @@ def test_evaluate_win_for_player1():
         [0, 2, 0]
     ]
     assert evaluate(board) == -10
+
+def test_evaluate_tie():
+    board = [
+        [1, 2, 1],
+        [2, 2, 1],
+        [1, 1, 2]
+    ]
+    assert evaluate(board) == 0
+
+def test_format_time():
+    assert format_time(3675) == "01:01:15"
+
+def test_minimax_blocks_win():
+    board = [
+        [1, 1, 0],
+        [0, 2, 0],
+        [0, 0, 2]
+    ]
+    best = find_best_move(board)
+    assert best == (0, 2)
